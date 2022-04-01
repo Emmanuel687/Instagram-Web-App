b4612cfb1abc85ef69ba573628c9b5297f0361b6
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-01 17:31:36 +0300

    Created a like & comment model properties and methods

--- instagram/models.py
+++ instagram/models.py
@@ -3,6 +3,8 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Image models properties
 class Image(models.Model):
     image = CloudinaryField('image', default='')
     name = models.CharField(max_length = 30,default='')
@@ -11,8 +13,7 @@ class Image(models.Model):
     likes = models.IntegerField(default=0)
     comments = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
-
-    
+# Image models Methods    
     def __str__(self):
         return self.name
 
@@ -31,3 +32,44 @@ class Image(models.Model):
     def update_caption(self, new_caption):
         self.image_caption = new_caption
         self.save()
+
+# Profile Models properties
+class Profile(models.Model):
+    profile_photo=CloudinaryField('image')
+    bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
+# Profile Models methods
+    def __str__(self):
+        return self.user.username
+
+    def update(self):
+        self.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_by_user(cls, user):
+        profile = cls.objects.filter(user=user)
+        return profile
+
+# Like Model properties
+class Like(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.likes
+
+# Comment Model properties
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=50)
+
+    def save_comment(self):
+        self.save()
+
+    def __str__(self):
+        return self.comment
