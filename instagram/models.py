f0cc3917474599e5b7e0ddca422f4f36b659d2c0
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-01 17:14:52 +0300

    Created a Image model with different properties

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Image(models.Model):
+    image = CloudinaryField('image', default='')
+    name = models.CharField(max_length = 30,default='')
+    caption = models.TextField()
+    profile = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+
+    
+    def __str__(self):
+        return self.name
