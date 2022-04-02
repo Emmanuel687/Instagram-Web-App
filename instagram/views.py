08c079d742b6c93a6cb212e0ab5b22e6b9a12e26
emmanuel687 <koechemmanuel2002@gmail.com>
2022-04-02 15:16:16 +0300

    Url settings re-configured in instagram/urls.py

--- instagram/views.py
+++ instagram/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
